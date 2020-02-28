25c74fa7505eb63db4d5b10a67b16d0b599e8bb6
Sem8 <mssemmi8@gmail.com>
2020-02-27 21:19:46 -0500

    successfully returns list of all complaints for ComplaintViewSet and successfully debugged issue with nested routers returning details not found bug after 6 and a half hours of debugging

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -2,11 +2,25 @@ from django.urls import path
 from rest_framework import routers
 from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
+router.register(r'^$', ComplaintViewSet, base_name='complaints')
 router.register(r'', ComplaintViewSet, base_name='complaint')
-router.register(r'openCases', OpenCasesViewSet, base_name='openCases')
-router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases')
-router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
+router.register(r'/openCases', OpenCasesViewSet, base_name='openCases')
+router.register(r'/closedCases', ClosedCasesViewSet, base_name='closedCases')
+router.register(r'/topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
 urlpatterns = [
+
 ]
 urlpatterns += router.urls
+
+
+# # My experimental code:
+# router.register(r'^$', HomeViewSet, base_name='home'),
+# router.register(r'^complaints/$', ComplaintViewSet, base_name='complaint'),
+# router.register(r'openCases', OpenCasesViewSet, base_name='openCases'),
+# router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases'),
+# router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints'),
+
+# urlpatterns = [
+# ]
+# urlpatterns += router.urls
--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -3,20 +3,63 @@ from .models import UserProfile, Complaint
 from .serializers import UserSerializer, UserProfileSerializer, ComplaintSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+from datetime import date
+
 # Create your views here.
+# class HomeViewSet(viewsets.ModelViewSet):
+#   queryset = UserProfile.objects.all()
+#   serializer_class = UserProfileSerializer(queryset, many=True)
+#     def list(self, request):
+#       return Response()
 
 class ComplaintViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
-  serializer_class = ComplaintSerializer
+  queryset = Complaint.objects.all()
+  # print('queryset:', queryset)
+  serializer_class = ComplaintSerializer(queryset, many=True)
   def list(self, request):
-    # Get all complaints from the user's district
-    return Response()
+    # Get all complaints from the user's district    
+    return Response(self.serializer_class.data)
+
+  # @action(methods=['get'], detail=True)
+  # def openCases(self, request):
+  #   newest = self.get_queryset().order_by('closedate').last()
+  #   queryset = self.get_queryset()
+
+    
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
+  first = Complaint.objects.all().order_by('closedate').last()
+  serializer = ComplaintSerializer(first, many=False)
+  # queryset = Complaint.objects.all()
+  # serializer_class = ComplaintSerializer(queryset, many=True)
+  # serializer_class = ComplaintSerializer
+  # def openCases(self, request):
+  # @action(methods=['get'], detail=True)
   def list(self, request):
-    # Get only the open complaints from the user's district
-    return Response()
+    return Response(self.serializer.data)
+    
+  #   # Get only the open complaints from the user's district
+  #   # newest = self.get_queryset().order_by('closedate').last()
+  #   # queryset = self.get_queryset()
+  #   t = date.today()
+  #   month = date.strftime(t)
+  #   year = t.year
+  #   title = f"MyClub Event Calendar {(month,year)}"
+
+  #   # serializer = self.get_serializer_class()(open)
+  #   # first_data = self.serializer_class.data[0]
+    
+  #   # open = None
+  #   # for open_cases in Complaint.objects.all():
+  #   #   if open_cases.opendate is not None:
+  #   #     open = 
+  #   # all = self.get_queryset()
+  #   # obj = get_object_or_404(all, pk=pk)
+  #   # return Response(serializer.data)
+
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get'] 
@@ -29,3 +72,11 @@ class TopComplaintTypeViewSet(viewsets.ModelViewSet):
   def list(self, request):
     # Get the top 3 complaint types from the user's district
     return Response()
+
+
+
+# class HomeViewSet(viewsets.ModelViewSet):
+#   queryset = UserProfile.objects.all()
+#   serializer_class = UserProfileSerializer(queryset, many=True)
+#     def list(self, request):
+#       return Response()
