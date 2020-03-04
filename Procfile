909ba7188d425df9b9f5ab275be42ce7edf5c39c
Sem8 <mssemmi8@gmail.com>
2020-03-03 23:53:14 -0500

    added weird python 2 unicode module in models file to troubleshoot heroku deployment issue

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: gunicorn testApp.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: gunicorn testApp.wsgi --log-file -
--- complaint_app/models.py
+++ complaint_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
+
 
 # Create your models here.
+@python_2_unicode_compatible
 class UserProfile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   full_name = models.CharField(max_length=150, blank=True, default="")
