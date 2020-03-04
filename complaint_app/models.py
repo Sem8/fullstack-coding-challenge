bcf022c322453085e3836dc70ee57e471905ab5e
Sem8 <mssemmi8@gmail.com>
2020-03-03 23:57:33 -0500

    change in syntax for python 2 unicode compatible

--- complaint_app/models.py
+++ complaint_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.six import python_2_unicode_compatible
+from django.utils.encoding import python_2_unicode_compatible
 
 
 # Create your models here.
